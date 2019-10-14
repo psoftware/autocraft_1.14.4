b97d210b1540a644e5f5064c26dc1359744cc934
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2019-10-14 03:26:03 +0200

    Recipe partially ported to 1.14: Ore Recipe classes are missing

--- src/main/java/com/theincgi/autocrafter/Recipe.java
+++ src/main/java/com/theincgi/autocrafter/Recipe.java
@@ -5,17 +5,17 @@
 
 package com.theincgi.autocrafter;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
+import net.minecraft.data.ShapedRecipeBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapedRecipes;
-import net.minecraft.item.crafting.ShapelessRecipes;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
+import net.minecraft.item.crafting.ShapedRecipe;
+import net.minecraft.item.crafting.ShapelessRecipe;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.crafting.CompoundIngredient;
@@ -28,30 +28,21 @@ public class Recipe {
     public NonNullList<Recipe.ItemOptions> items;
 
     public Recipe() {
-        this.items = NonNullList.func_191197_a(9, Recipe.ItemOptions.EMPTY);
+        this.items = NonNullList.withSize(9, Recipe.ItemOptions.EMPTY);
     }
 
     public void setRecipe(IRecipe iRecipe) {
-        this.output = iRecipe.func_77571_b();
+        this.output = iRecipe.getRecipeOutput();
 
-        for(int i = 0; i < this.items.size(); ++i) {
+        for(int i = 0; i < this.items.size(); ++i)
             this.items.set(i, Recipe.ItemOptions.EMPTY);
-        }
 
-        int i;
-        int j;
-        if (iRecipe instanceof ShapedRecipes) {
-            ShapedRecipes sr = (ShapedRecipes)iRecipe;
-
-            for(i = 0; i < sr.field_77574_d.size(); ++i) {
-                ItemStack[] s = ((Ingredient)sr.field_77574_d.get(i)).func_193365_a();
-                ArrayList<ItemStack> alis = new ArrayList();
-
-                for(j = 0; j < s.length; ++j) {
-                    alis.add(s[j]);
-                }
-
-                this.items.set(this.realIndx(i, sr.field_77576_b, sr.field_77577_c), new Recipe.ItemOptions(alis));
+        if (iRecipe instanceof ShapedRecipe) {
+            ShapedRecipe sr = (ShapedRecipe)iRecipe;
+            
+            for(int i = 0; i < sr.getIngredients().size(); ++i) {
+                ArrayList<ItemStack> alis = new ArrayList<>(Arrays.asList(sr.getIngredients().get(i).getMatchingStacks()));
+                this.items.set(this.realIndx(i, sr.getRecipeWidth(), sr.getRecipeHeight()), new Recipe.ItemOptions(alis));
             }
         } else {
             Object o;
@@ -59,59 +50,54 @@ public class Recipe {
             OreIngredient oreIngredient;
             CompoundIngredient ci;
             Ingredient ing;
+
             if (iRecipe instanceof ShapedOreRecipe) {
                 ShapedOreRecipe sor = (ShapedOreRecipe)iRecipe;
 
-                for(i = 0; i < sor.func_192400_c().size(); ++i) {
+                for(int i = 0; i < sor.func_192400_c().size(); ++i) {
                     o = sor.func_192400_c().get(i);
                     if (o == null) {
-                        o = ItemStack.field_190927_a;
+                        o = ItemStack.EMPTY;
                     }
 
-                    if (o instanceof List) {
+                    if (o instanceof List)
                         this.items.set(this.realIndx(i, sor.getWidth(), sor.getHeight()), new Recipe.ItemOptions((List)o));
-                    } else if (o instanceof ItemStack) {
-                        is = (ItemStack)o;
-                        this.items.set(this.realIndx(i, sor.getWidth(), sor.getHeight()), new Recipe.ItemOptions(is));
-                    } else if (o instanceof OreIngredient) {
-                        oreIngredient = (OreIngredient)o;
-                        this.items.set(i, new Recipe.ItemOptions(oreIngredient));
-                    } else if (o instanceof CompoundIngredient) {
-                        ci = (CompoundIngredient)o;
-                        this.items.set(i, new Recipe.ItemOptions(ci));
-                    } else if (o instanceof Ingredient) {
-                        ing = (Ingredient)o;
-                        this.items.set(i, new Recipe.ItemOptions(ing));
-                    } else {
-                        this.output = ItemStack.field_190927_a;
+                    else if (o instanceof ItemStack)
+                        this.items.set(this.realIndx(i, sor.getWidth(), sor.getHeight()), new Recipe.ItemOptions((ItemStack)o));
+                    else if (o instanceof OreIngredient)
+                        this.items.set(i, new Recipe.ItemOptions((OreIngredient)o));
+                    else if (o instanceof CompoundIngredient)
+                        this.items.set(i, new Recipe.ItemOptions((CompoundIngredient)o));
+                    else if (o instanceof Ingredient)
+                        this.items.set(i, new Recipe.ItemOptions((Ingredient)o));
+                    else {
+                        this.output = ItemStack.EMPTY;
                         Utils.log("AutoCrafter: ShapedOreRecipe missing case '" + o.getClass() + "'");
                     }
                 }
-            } else if (iRecipe instanceof ShapelessRecipes) {
-                ShapelessRecipes sr = (ShapelessRecipes)iRecipe;
+            } else if (iRecipe instanceof ShapelessRecipe) {
+                ShapelessRecipe sr = (ShapelessRecipe)iRecipe;
 
-                for(i = 0; i < sr.field_77579_b.size(); ++i) {
-                    ArrayList<ItemStack> ios = new ArrayList();
-                    ItemStack[] isa = ((Ingredient)sr.field_77579_b.get(i)).func_193365_a();
+                for(int i = 0; i < sr.getIngredients().size(); ++i) {
+                    ItemStack[] isa = sr.getIngredients().get(i).getMatchingStacks();
 
-                    for(j = 0; j < isa.length; ++j) {
-                        ios.add(isa[j]);
-                    }
+                    ArrayList<ItemStack> ios = new ArrayList<>();
+                    ios.addAll(Arrays.asList(isa));
 
                     this.items.set(i, new Recipe.ItemOptions(ios));
                 }
             } else if (iRecipe instanceof ShapelessOreRecipe) {
                 ShapelessOreRecipe slor = (ShapelessOreRecipe)iRecipe;
 
-                for(i = 0; i < slor.func_192400_c().size(); ++i) {
+                for(int i = 0; i < slor.func_192400_c().size(); ++i) {
                     o = slor.func_192400_c().get(i);
                     if (o == null) {
-                        o = ItemStack.field_190927_a;
+                        o = ItemStack.EMPTY;
                     }
 
-                    if (o instanceof List) {
+                    if (o instanceof List)
                         this.items.set(i, new Recipe.ItemOptions((List)o));
-                    } else if (o instanceof ItemStack) {
+                    else if (o instanceof ItemStack) {
                         is = (ItemStack)o;
                         this.items.set(i, new Recipe.ItemOptions(is));
                     } else if (o instanceof OreIngredient) {
@@ -124,12 +110,12 @@ public class Recipe {
                         ing = (Ingredient)o;
                         this.items.set(i, new Recipe.ItemOptions(ing));
                     } else {
-                        this.output = ItemStack.field_190927_a;
+                        this.output = ItemStack.EMPTY;
                         Utils.log("AutoCrafter: ShaplessOreRecipe missing case '" + o.getClass() + "'");
                     }
                 }
             } else {
-                this.output = ItemStack.field_190927_a;
+                this.output = ItemStack.EMPTY;
                 Utils.log("It seems " + iRecipe.getClass().toGenericString() + " isn't a supported recipe type.");
             }
         }
@@ -145,56 +131,53 @@ public class Recipe {
     public ItemStack getDisplayItem(int slot) {
         Recipe.ItemOptions opt = (Recipe.ItemOptions)this.items.get(slot);
         if (opt.opts.size() == 0) {
-            return ItemStack.field_190927_a;
+            return ItemStack.EMPTY;
         } else {
             long mod = System.currentTimeMillis() / 500L % (long)opt.opts.size();
             ItemStack is = (ItemStack)opt.opts.get((int)mod);
-            return new ItemStack(is.func_77973_b(), 1, is.func_77952_i() == 32767 ? (int)mod % Math.max(1, is.func_77958_k()) : is.func_77952_i());
+            ItemStack itemstack = new ItemStack(is.getItem(), 1);
+            itemstack.setDamage(is.getDamage() == 32767 ? (int)mod % Math.max(1, is.getMaxDamage()) : is.getDamage());
+            return itemstack;
         }
     }
 
     public boolean matchesRecipe(int slot, ItemStack itemStack) {
         Recipe.ItemOptions opt = (Recipe.ItemOptions)this.items.get(slot);
-        if (opt.opts.size() == 0 && itemStack.func_190926_b()) {
+        if (opt.opts.size() == 0 && itemStack.isEmpty()) {
             return true;
         } else {
             Iterator var4 = opt.opts.iterator();
 
-            ItemStack i;
-            do {
-                if (!var4.hasNext()) {
-                    return false;
-                }
-
-                i = (ItemStack)var4.next();
-                Item item1 = i.func_77973_b();
-                Item item2 = itemStack.func_77973_b();
-            } while(!matches(i, itemStack));
-
-            return true;
+            for(ItemStack i : opt.opts){
+                Item item1 = i.getItem();
+                Item item2 = itemStack.getItem();
+                if(matches(i, itemStack))
+                    return true;
+            }
+            return false;
         }
     }
 
-    public NBTTagList getNBT() {
-        NBTTagList list = new NBTTagList();
-        list.func_74742_a(this.getOutput().serializeNBT());
+    public ListNBT getNBT() {
+        ListNBT list = new ListNBT();
+        list.add(this.getOutput().serializeNBT());
 
         for(int i = 0; i < this.items.size(); ++i) {
-            NBTTagCompound slot = new NBTTagCompound();
-            slot.func_74782_a("item", ((Recipe.ItemOptions)this.items.get(i)).getNBT());
-            list.func_74742_a(slot);
+            CompoundNBT slot = new CompoundNBT();
+            slot.put("item", ((Recipe.ItemOptions)this.items.get(i)).getNBT());
+            list.add(slot);
         }
 
         return list;
     }
 
-    public static Recipe fromNBT(NBTTagList tags) {
+    public static Recipe fromNBT(ListNBT tags) {
         Recipe recipe = new Recipe();
-        recipe.output = new ItemStack(tags.func_150305_b(0));
+        recipe.output = ItemStack.read(tags.getCompound(0));
 
-        for(int i = 1; i < tags.func_74745_c(); ++i) {
-            NBTTagCompound slot = tags.func_150305_b(i);
-            Recipe.ItemOptions opts = Recipe.ItemOptions.fromNBT(slot.func_150295_c("item", 10));
+        for(int i = 1; i < tags.size(); ++i) {
+            CompoundNBT slot = tags.getCompound(i);
+            Recipe.ItemOptions opts = Recipe.ItemOptions.fromNBT(slot.getList("item", 10));
             recipe.items.set(i - 1, opts);
         }
 
@@ -206,7 +189,7 @@ public class Recipe {
             this.items.set(i, Recipe.ItemOptions.EMPTY);
         }
 
-        this.output = ItemStack.field_190927_a;
+        this.output = ItemStack.EMPTY;
     }
 
     public String toString() {
@@ -217,8 +200,8 @@ public class Recipe {
             s = s + "\ti: " + i + " = {";
 
             for(int j = 0; j < opts.opts.size(); ++j) {
-                s = s + ((ItemStack)opts.opts.get(j)).func_77973_b().getRegistryName().toString() + " : ";
-                s = s + ((ItemStack)opts.opts.get(j)).func_77952_i();
+                s = s + ((ItemStack)opts.opts.get(j)).getItem().getRegistryName().toString() + " : ";
+                s = s + ((ItemStack)opts.opts.get(j)).getDamage();
                 if (j < opts.opts.size() - 1) {
                     s = s + ", ";
                 }
@@ -234,15 +217,14 @@ public class Recipe {
     }
 
     public static boolean matches(ItemStack crafts, ItemStack stack) {
-        if (crafts.func_190926_b() && stack.func_190926_b()) {
+        if (crafts.isEmpty() && stack.isEmpty())
             return true;
-        } else if (!crafts.func_77973_b().equals(stack.func_77973_b())) {
+        else if (!crafts.getItem().equals(stack.getItem()))
             return false;
-        } else if (crafts.func_77952_i() != 32767 && stack.func_77952_i() != 32767) {
-            return crafts.func_77952_i() == stack.func_77952_i();
-        } else {
+        else if (crafts.getDamage() != 32767 && stack.getDamage() != 32767)
+            return crafts.getDamage() == stack.getDamage();
+        else
             return true;
-        }
     }
 
     public boolean equals(Object obj) {
@@ -264,7 +246,7 @@ public class Recipe {
     }
 
     public NonNullList<ItemStack> getLeftovers(NonNullList<ItemStack> inv, int from, int to) {
-        NonNullList<ItemStack> ret = NonNullList.func_191197_a(to - from, ItemStack.field_190927_a);
+        NonNullList<ItemStack> ret = NonNullList.withSize(to - from, ItemStack.EMPTY);
 
         for(int i = from; i < to; ++i) {
             ret.set(i, ForgeHooks.getContainerItem((ItemStack)inv.get(i)));
@@ -274,7 +256,7 @@ public class Recipe {
     }
 
     public ItemStack getOutput() {
-        return this.output == null ? ItemStack.field_190927_a : this.output.func_77946_l();
+        return this.output == null ? ItemStack.EMPTY : this.output.copy();
     }
 
     public static class ItemOptions {
@@ -282,15 +264,15 @@ public class Recipe {
         NonNullList<ItemStack> opts;
 
         public ItemOptions() {
-            this.opts = NonNullList.func_191196_a();
+            this.opts = NonNullList.create();
         }
 
-        public static Recipe.ItemOptions fromNBT(NBTTagList tagList) {
+        public static Recipe.ItemOptions fromNBT(ListNBT tagList) {
             Recipe.ItemOptions opts = new Recipe.ItemOptions();
 
-            for(int i = 0; i < tagList.func_74745_c(); ++i) {
-                NBTTagCompound itemTag = tagList.func_150305_b(i);
-                opts.opts.add(new ItemStack(itemTag));
+            for(int i = 0; i < tagList.size(); ++i) {
+                CompoundNBT itemTag = tagList.getCompound(i);
+                opts.opts.add(ItemStack.read(itemTag));
             }
 
             return opts;
@@ -301,34 +283,30 @@ public class Recipe {
         }
 
         public ItemOptions(ItemStack[] matching) {
-            this.opts = NonNullList.func_191196_a();
-
-            for(int j = 0; j < matching.length; ++j) {
-                this.opts.add(matching[j]);
-            }
-
+            this.opts = NonNullList.create();
+            Collections.addAll(this.opts, matching);
         }
 
         public ItemOptions(OreIngredient i) {
-            this(i.func_193365_a());
+            this(i.getMatchingStacks());
         }
 
         public ItemOptions(CompoundIngredient ci) {
-            this(ci.func_193365_a());
+            this(ci.getMatchingStacks());
         }
 
         public ItemOptions(ItemStack itemStack) {
-            this.opts = NonNullList.func_191196_a();
+            this.opts = NonNullList.create();
             this.opts.add(itemStack);
         }
 
         public ItemOptions(NonNullList<ItemStack> opts) {
-            this.opts = NonNullList.func_191196_a();
+            this.opts = NonNullList.create();
             this.opts = opts;
         }
 
         public ItemOptions(List<ItemStack> opts) {
-            this.opts = NonNullList.func_191196_a();
+            this.opts = NonNullList.create();
 
             for(int i = 0; i < opts.size(); ++i) {
                 this.opts.add(opts.get(i));
@@ -337,8 +315,8 @@ public class Recipe {
         }
 
         public ItemOptions(Ingredient ing) {
-            this.opts = NonNullList.func_191196_a();
-            ItemStack[] var2 = ing.func_193365_a();
+            this.opts = NonNullList.create();
+            ItemStack[] var2 = ing.getMatchingStacks();
             int var3 = var2.length;
 
             for(int var4 = 0; var4 < var3; ++var4) {
@@ -348,11 +326,11 @@ public class Recipe {
 
         }
 
-        public NBTTagList getNBT() {
-            NBTTagList list = new NBTTagList();
+        public ListNBT getNBT() {
+            ListNBT list = new ListNBT();
 
             for(int i = 0; i < this.opts.size(); ++i) {
-                list.func_74742_a(((ItemStack)this.opts.get(i)).serializeNBT());
+                list.add(((ItemStack)this.opts.get(i)).serializeNBT());
             }
 
             return list;
